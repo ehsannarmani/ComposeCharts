1b04ccd5d2acf3da4303c55a31108ecb203d6dee
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2024-09-21 11:41:26 +0330

    increase version

--- compose-charts/build.gradle.kts
+++ compose-charts/build.gradle.kts
@@ -15,7 +15,7 @@ mavenPublishing{
     coordinates(
         groupId = "io.github.ehsannarmani",
         artifactId = "compose-charts",
-        version = "0.0.13"
+        version = "0.0.14"
     )
     pom{
         name.set("Compose Charts")
