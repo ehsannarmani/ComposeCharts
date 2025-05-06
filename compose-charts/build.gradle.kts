53af0f206ce89c151e0c623792a627d60c74042e
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2025-05-07 00:02:49 +0330

    bump version

--- compose-charts/build.gradle.kts
+++ compose-charts/build.gradle.kts
@@ -15,7 +15,7 @@ mavenPublishing{
     coordinates(
         groupId = "io.github.ehsannarmani",
         artifactId = "compose-charts",
-        version = "0.1.2"
+        version = "0.1.5"
     )
     pom{
         name.set("Compose Charts")
