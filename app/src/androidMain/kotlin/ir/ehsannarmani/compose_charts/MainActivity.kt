483f973eb70cd0de377e9a128bbd5643704755f5
Sean Proctor <sproctor@gmail.com>
2026-02-17 09:14:38 -0500

    Unify android theme with other targets and simplify layout

--- app/src/androidMain/kotlin/ir/ehsannarmani/compose_charts/MainActivity.kt
+++ app/src/androidMain/kotlin/ir/ehsannarmani/compose_charts/MainActivity.kt
@@ -5,19 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import ir.ehsannarmani.compose_charts.ui.App
-import ir.ehsannarmani.compose_charts.ui.theme.ComposeChartsTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         enableEdgeToEdge()
         setContent {
-            ComposeChartsTheme(false) {
-                App()
-            }
+            App()
         }
     }
 }
