ca0af89ac641bc8d3efcae3af397ecc41991d4db
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2026-07-12 00:27:46 +0330

    Improve pie click detection and fix issues on stroke mode

--- compose-charts/src/commonMain/kotlin/ir/ehsannarmani/compose_charts/extensions/Circle.kt
+++ compose-charts/src/commonMain/kotlin/ir/ehsannarmani/compose_charts/extensions/Circle.kt
@@ -1,13 +1,30 @@
 package ir.ehsannarmani.compose_charts.extensions
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.Density
+import ir.ehsannarmani.compose_charts.models.Pie
 import kotlin.math.pow
 
-fun isInsideCircle(touchTapOffset: Offset, pieceOffset: Offset, radius: Float): Boolean {
-    // Calculate the squared distance between the touch point and the circle's center
-    val distanceSquared =
-        (touchTapOffset.x - pieceOffset.x).pow(2) + (touchTapOffset.y - pieceOffset.y).pow(2)
 
-    // Compare the squared distance with the squared radius to avoid using sqrt (more efficient)
-    return distanceSquared <= radius.pow(2)
+internal fun Density.isInsidePie(
+    touchTapOffset: Offset,
+    pieceOffset: Offset,
+    radius: Float,
+    style: Pie.Style?,
+): Boolean {
+    val distanceSquared =
+        (touchTapOffset.x - pieceOffset.x).pow(2) +
+                (touchTapOffset.y - pieceOffset.y).pow(2)
+
+    return when (style) {
+        is Pie.Style.Stroke -> {
+            val half = style.width.toPx() / 2f
+            val inner = (radius - half).coerceAtLeast(0f)
+            val outer = radius + half
+            distanceSquared in (inner.pow(2))..(outer.pow(2))
+        }
+        else -> {
+            distanceSquared <= radius.pow(2)
+        }
+    }
 }
